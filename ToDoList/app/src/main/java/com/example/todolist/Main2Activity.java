54a7e4146d132e738a77f1d509d3c7ab8887cd0b
unknown <gedekresnap.gmail.com>
2020-09-30 21:08:51 +0700

    pindah layout

---
+++ ToDoList/app/src/main/java/com/example/todolist/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_delete_layout);
+    }
+}
--- ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+
+    ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    button = (Button) findViewById(R.id.btnNextPage);
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openActivity2();
+        }
+    });
+
+        //to do list
+        listview=(ListView)findViewById(R.id.lvItems);
+
+        ArrayList<String> arrayList=new ArrayList<>();
+
+        arrayList.add("Tugas WPPL");
+        arrayList.add("Workout");
+        arrayList.add("Radep ngonsep PKO");
+        arrayList.add("Take video PKO");
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+
+        listview.setAdapter(arrayAdapter);
     }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+
 }
